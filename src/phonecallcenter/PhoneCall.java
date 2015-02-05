5c270c43248d50171fbe03e7c192e7f8e91b213c
Rafael Andara <rafael.andara@gmail.com>
2015-02-05 15:52:31 -0500

    ArrayList for calls and tasks

--- src/phonecallcenter/PhoneCall.java
+++ src/phonecallcenter/PhoneCall.java
@@ -8,22 +8,37 @@ public class PhoneCall implements Callable<Boolean> {
 	private PhoneCallManager manager;
 	private String from;
 	private String to;
+	private boolean processCall;
 	
-	public PhoneCall(String from, String to, int duration) {
+	public PhoneCall(String from, String to, int duration, boolean process) {
 		manager = PhoneCallManager.getInstance();
 		this.from = from;
 		this.to = to;
 		this.callDuration = duration;
+		this.processCall = process;
 	}
 	
 	@Override
 	public Boolean call() throws Exception {
-		Thread.sleep(callDuration);
-		manager.phoneCallHappened(this.from, this.to);
+		if(this.processCall) {
+			Thread.sleep(callDuration);
+			manager.phoneCallHappened(this.from, this.to);
+		}
+		else{
+			System.out.println(this.toString()+" was canceled.");
+		}
 		return Boolean.TRUE;
 	}
 	
 	public String toString() {
 		return "call from: "+this.from +" to: "+this.to;
+	}
+	
+	public String getFrom() {
+		return this.from;
+	}
+	
+	public String getTo() {
+		return this.to;
 	}
 }
--- src/phonecallcenter/PhoneCallCenter.java
+++ src/phonecallcenter/PhoneCallCenter.java
@@ -1,5 +1,6 @@
 package phonecallcenter;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
@@ -10,33 +11,44 @@ public class PhoneCallCenter {
 		
 		PhoneCallManager manager = PhoneCallManager.getInstance();
 		
-		PhoneCall call1 = new PhoneCall("a","b",5000);
-		PhoneCall call2 = new PhoneCall("b","a",5000);
-		PhoneCall call3 = new PhoneCall("x","y",5000);
-		PhoneCall call4 = new PhoneCall("a","y",5000);
+		ArrayList<PhoneCall> callList = new ArrayList<PhoneCall>();
+		ArrayList<FutureTask<Boolean>> taskList = new ArrayList<FutureTask<Boolean>>();
 		
-		FutureTask<Boolean> futureTask1 = new FutureTask<Boolean>(call1);
-		FutureTask<Boolean> futureTask2 = new FutureTask<Boolean>(call2);
-		FutureTask<Boolean> futureTask3 = new FutureTask<Boolean>(call3);
+		callList.add(new PhoneCall("a", "b", 5000, true));
+		callList.add(new PhoneCall("b", "a", 2000, true));
+		callList.add(new PhoneCall("x", "y", 2000, true));
+		callList.add(new PhoneCall("a", "y", 5000, false)); // call to be canceled
+		
+		for(PhoneCall call : callList) {
+			taskList.add(new FutureTask<Boolean>(call));
+		}
 		
 		ExecutorService executor = Executors.newFixedThreadPool(3);
-		executor.execute(futureTask1);
-		executor.execute(futureTask2);
-		executor.execute(futureTask3);
+		
+		for(FutureTask<Boolean> task : taskList) {
+			executor.execute(task);
+		}
 		
 		System.out.println("Waiting for calls...");
 		
+		boolean allCallsDone;
+		
 		while(true) {
 			try {
-				if(futureTask1.isDone() && futureTask2.isDone() && futureTask3.isDone()) {
+				allCallsDone = true;
+				
+				for(FutureTask<Boolean> task : taskList) {
+					if(!task.isDone()) {
+						allCallsDone = false;
+					}
+				}
+				if(allCallsDone) {
 					System.out.println("All calls are Done");
-					
 					System.out.println("Checking with PhoneCallManager ...");
-					System.out.println("Did " +call1.toString()+ " happen? "+ manager.didPhoneCallHappen("a", "b"));
-					System.out.println("Did " +call2.toString()+ " happen? "+ manager.didPhoneCallHappen("b", "a"));
-					System.out.println("Did " +call3.toString()+ " happen? "+ manager.didPhoneCallHappen("x", "y"));
-					System.out.println("Did " +call4.toString()+ " happen? "+ manager.didPhoneCallHappen("a", "y"));
 					
+					for(PhoneCall call : callList) {
+						System.out.println("Did " +call.toString()+ " happen? "+ manager.didPhoneCallHappen(call.getFrom(), call.getTo()));
+					}
 					executor.shutdown();
 					return;
 				}
